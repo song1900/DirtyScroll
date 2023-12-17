b0480d3d7beda3c97d5773ac1e8d2770f25ab4b3
song1900 <woojin1900@gmail.com>
2023-12-17 23:06:55 +0900

    Feat: View Overlay

--- DirtyScroll/ContentView.swift
+++ DirtyScroll/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .dirtyScrollOverlay(Color.orange)
     }
 }
 
---
+++ DirtyScroll/DirtyScrollModifier.swift
@@ -0,0 +1,33 @@
+//
+//  DirtyScrollModifier.swift
+//  DirtyScroll
+//
+//  Created by denver on 2023/12/17.
+//
+
+import SwiftUI
+
+public extension View {
+    
+    /// 뷰에 overlay를 추가 합니다.
+    ///
+    /// - parameter content: overlay의 내용입니다.
+    ///
+    /// - returns: 해당 뷰 위에 overlay 추가된 뷰를 반환합니다.
+    func dirtyScrollOverlay<Content: View>(_ content: Content) -> some View {
+        modifier(AddDirtyScrollOverlayModifier(overlay: content))
+    }
+    
+    
+}
+
+public struct AddDirtyScrollOverlayModifier<Overlay: View>: ViewModifier {
+
+    let overlay: Overlay
+
+    // MARK: - ViewModifier
+
+    public func body(content: Content) -> some View {
+        Color.blue
+    }
+}
