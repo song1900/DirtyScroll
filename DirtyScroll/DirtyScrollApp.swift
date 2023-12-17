776b2cf2a98e4d198fea83e3f599135ec10563eb
denver <denver@jejememe.com>
2023-12-17 22:27:59 +0900

    Initial Commit

---
+++ DirtyScroll/DirtyScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirtyScrollApp.swift
+//  DirtyScroll
+//
+//  Created by denver on 2023/12/17.
+//
+
+import SwiftUI
+
+@main
+struct DirtyScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
